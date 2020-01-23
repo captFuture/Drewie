214c15c5558dd30ce3a71c5ba2284fb898d4990e
Christian Thomas <info@webgyver.de>
2020-01-23 22:29:42 +0100

    replace socket.io

--- server/index.js
+++ server/index.js
@@ -22,97 +22,99 @@ const config = require('../lib/json-store')(CONFIG_PATH);
 const drewie = require('../drewie')(config);
 
 const app = express();
-const srv = http.createServer(app);
-const wss = new WebSocket.Server({ clientTracking: false, noServer: true });
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 app.use(serve(PUBLIC_PATH));
 
-const broadcast = message =>
-  wss.clients.filter(client => client.readyState === WebSocket.OPEN).forEach(client => client.send(message));
-
-wss.on('connection', (ws, req) => {
-  /*
-  const emit = (topic, payload = null) => ws.send(JSON.stringify({ topic, payload }))
-  ws.on('message', message => {})
-
-  // old
-  console.log('connection!');
-  socket.emit('connected', { hello: 'world' });
-  socket.emit('botConnectionStatus', { connected: true });
-
-  socket.on('pen', function(data) {
-    c.pen(data.up);
-  });
-
-  socket.on('r', function(data) {
-    c.rotate(Number(data.m), Number(data.dir), Number(data.d), Number(data.steps));
-    //console.log(data.d, Number(data.x), Number(data.y))
-    //c.moveRelative(data.x, data.y)
-  });
-
-  socket.on('drawpath', function(data) {
-    c.addPath(data.path);
-  });
-  socket.on('drawart', function(data) {
-    // Get whole svg, extract all path tags and concatenate them
-    var $ = cheerio.load(data.content, { xmlMode: true });
+const wrapper = {
+  getDXY() {
+    this.send('DXY', {
+      d: drewie._D,
+      x: drewie.startPos.x,
+      y: drewie.startPos.y,
+      s: drewie.drawingScale,
+      limx: drewie.limits.x,
+      limy: drewie.limits.y,
+      strings: drewie.startStringLengths,
+    });
+  },
+  pen({ up }) {
+    drewie.pen(up);
+  },
+  r(data) {
+    drewie.rotate(Number(data.m), Number(data.dir), Number(data.d), Number(data.steps));
+  },
+  drawpath({ path }) {
+    drewie.addPath(path);
+  },
+  drawart({ content }) {
+    var $ = cheerio.load(content, { xmlMode: true });
     var fullpath = '';
     $('path').each(function() {
       var d = $(this).attr('d');
       fullpath += d.replace(/\s+/g, ' ') + ' ';
     });
 
-    c.paths = [];
-    c.drawingPath = false;
-    c.addPath(fullpath.trim());
+    drewie.paths = [];
+    drewie.drawingPath = false;
+    drewie.addPath(fullpath.trim());
     //console.log(fullpath.trim())
-  });
-  socket.on('setStartPos', function(data) {
-    c.setStartPos(data);
-  });
-  socket.on('drawingScale', function(data) {
-    c.setDrawingScale(data.drawingScale);
+  },
+  setStartPos(pos) {
+    drewie.setStartPos(pos);
+  },
+  drawingScale(data) {
+    drewie.setDrawingScale(data.drawingScale);
     console.log('setscale:' + data.drawingScale);
-  });
-  socket.on('setD', function(data) {
-    c.setD(Number(data.d));
-  });
-  socket.on('setScale', function(data) {
-    c.setScale(Number(data.s));
+  },
+  setD(data) {
+    drewie.setD(Number(data.d));
+  },
+  setScale(data) {
+    drewie.setScale(Number(data.s));
     console.log('setScale:' + data.s);
+  },
+  moveto(data) {
+    drewie.moveTo(data.x, data.y);
+  },
+  pause(data) {
+    drewie.pause();
+  },
+  reboot(data) {
+    drewie.reboot();
+  },
+  clearCanvas(data) {
+    drewie.clearcanvas();
+  },
+};
+
+const wsMessageHandler = ws => {
+  const ctx = {
+    send: (topic, payload = null) =>
+      wss.clients.forEach(
+        client => client.readyState === WebSocket.OPEN && client.send(JSON.stringify({ topic, payload }))
+      ),
+  };
+
+  ws.on('message', msg => {
+    try {
+      const { topic, payload } = JSON.parse(msg);
+      wrapper[topic].call(ctx, payload);
+    } catch (e) {
+      console.log(e, msg);
+    }
   });
 
-  socket.on('moveto', function(data) {
-    c.moveTo(data.x, data.y);
-  });
+  ctx.send('connected', { hello: 'world' });
+  ctx.send('botConnectionStatus', { connected: true });
+};
 
-  socket.on('getDXY', function(data) {
-    socket.emit('DXY', {
-      d: c._D,
-      x: c.startPos.x,
-      y: c.startPos.y,
-      s: c.drawingScale,
-      limx: c.limits.x,
-      limy: c.limits.y,
-      strings: c.startStringLengths,
-    });
-  });
-
-  socket.on('pause', function(data) {
-    c.pause();
-  });
-
-  socket.on('reboot', function(data) {
-    c.reboot();
-  });
-
-  socket.on('clearCanvas', function(data) {
-    c.clearcanvas();
-  });
-  */
+wss.on('connection', (ws, req) => {
+  wsMessageHandler(ws);
 });
 
-srv.listen(config.data.localPort, async () => {
+server.listen(config.data.localPort, async () => {
   console.log(`listening on port ${config.data.localPort}...`);
 
   console.log('pentest');
